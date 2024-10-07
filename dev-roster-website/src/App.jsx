4e392406607c60c959622ebd6daaf23e08d96b89
leann-dinh <145501652+ldinh67@users.noreply.github.com>
2024-10-06 21:00:35 -0400

    add name of new england innocence project team members

--- dev-roster-website/src/App.jsx
+++ dev-roster-website/src/App.jsx
@@ -21,6 +21,10 @@ function App() {
           <li>Test Developer Name</li>
           <li>Test Developer Name</li>
         </ul>
+        <h2>New England Innocence Project</h2>
+        <ul>
+          <li>Test Developer Name: Leann, Brandon, Dhruvii, Eleanor, Henru, Kevin, Lizzie, Mallory, Marten, Nelson, Sara, Siara, Kelly</li>
+        </ul>
       </div>
     </>
   )
